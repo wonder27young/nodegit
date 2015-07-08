a9c6ff63990c5cccd0cbc313f1439b30e7a4e4dc
Maximiliano Korp <maxkorp@8bytealchemy.com>
2015-07-08 07:56:35 -0700

    make generate return a promise

--- generate/index.js
+++ generate/index.js
@@ -1,11 +1,21 @@
+var Promise = require("nodegit-promise");
+
 var generateJson = require("./scripts/generateJson");
 var generateNativeCode = require("./scripts/generateNativeCode");
 var generateMissingTests = require("./scripts/generateMissingTests");
 
 module.exports = function generate() {
-  generateJson();
-  generateNativeCode();
-  generateMissingTests();
+  return new Promise(function(resolve, reject) {
+    try {
+      generateJson();
+      generateNativeCode();
+      generateMissingTests();
+      resolve();
+    }
+    catch(e) {
+      reject(e);
+    }
+  });
 }
 
 if (require.main === module) {
